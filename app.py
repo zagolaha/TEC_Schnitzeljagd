5dcf92b550be199a7e90d2ceccbce0c9610fa0e0
Hagen Zagola <hagen.zagola@trumpf.com>
2023-11-02 08:41:16 +0100

    Update branch

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ from flask import request
 app = Flask(__name__)
 
 # styling components
-answer_btn = "text-5xl rounded-xl min-h-fit w-3/4 p-10 bg-Lightgray font-semibold font-roboto"
+answer_btn = "text-5xl rounded-xl min-h-fit w-3/4 p-10 bg-Lightgray font-semibold"
 answers = ["24 000", "2 453", "4 500", "7 500"]
 
 questions = json.load(open("static/questions.json"))
@@ -24,4 +24,4 @@ def scan():
     return render_template("scan.html")
 
 if __name__ == '__main__':
-   app.run(debug = True)
+   app.run(ssl_context='adhoc')
